9b850cad0fe4388cdab942758a793e35074f31a4
6pkrg8k8d6-hub <6pkrg8k8d6@privaterelay.appleid.com>
2026-07-30 14:03:28 +1000

    Create heroes.js

---
+++ css/js/heroes.js
@@ -0,0 +1,63 @@
+const heroes = [
+
+{
+name:"Axe",
+role:"3",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/axe.png"
+},
+
+{
+name:"Centaur Warrunner",
+role:"3",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/centaur.png"
+},
+
+{
+name:"Doom",
+role:"3",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/doom.png"
+},
+
+{
+name:"Magnus",
+role:"3",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/magnataur.png"
+},
+
+{
+name:"Tiny",
+role:"3/4",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png"
+},
+
+{
+name:"Wraith King",
+role:"1/3",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/skeleton_king.png"
+},
+
+{
+name:"Phantom Lancer",
+role:"1",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/phantom_lancer.png"
+},
+
+{
+name:"Naga Siren",
+role:"1",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/naga_siren.png"
+},
+
+{
+name:"Terrorblade",
+role:"1",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/terrorblade.png"
+},
+
+{
+name:"Storm Spirit",
+role:"2",
+image:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/storm_spirit.png"
+}
+
+];
