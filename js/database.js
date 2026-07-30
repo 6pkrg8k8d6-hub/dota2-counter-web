35b4a9a621752f53a514a25eb13074954dd9a13a
6pkrg8k8d6-hub <6pkrg8k8d6@privaterelay.appleid.com>
2026-07-30 14:25:31 +1000

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,157 @@
+const heroes = [
+
+{
+name:"Axe",
+position:[3],
+image:"axe",
+counters:{
+strong:[
+"Phantom Lancer",
+"Naga Siren",
+"Terrorblade",
+"Chaos Knight",
+"Meepo"
+],
+weak:[
+"Timbersaw",
+"Necrophos",
+"Viper",
+"Monkey King"
+]
+}
+},
+
+
+{
+name:"Centaur Warrunner",
+position:[3],
+image:"centaur",
+counters:{
+strong:[
+"Anti-Mage",
+"Slark",
+"Phantom Assassin",
+"Ursa"
+],
+weak:[
+"Monkey King",
+"Viper",
+"Timbersaw"
+]
+}
+},
+
+
+{
+name:"Doom",
+position:[3],
+image:"doom",
+counters:{
+strong:[
+"Storm Spirit",
+"Ember Spirit",
+"Puck",
+"Morphling",
+"Medusa"
+],
+weak:[
+"Silencer",
+"Viper",
+"Necrophos"
+]
+}
+},
+
+
+{
+name:"Magnus",
+position:[3,4],
+image:"magnataur",
+counters:{
+strong:[
+"Sven",
+"Troll Warlord",
+"Phantom Assassin",
+"Juggernaut"
+],
+weak:[
+"Silencer",
+"Rubick",
+"Viper"
+]
+}
+},
+
+
+{
+name:"Tiny",
+position:[3,4],
+image:"tiny",
+counters:{
+strong:[
+"Sniper",
+"Drow Ranger",
+"Crystal Maiden"
+],
+weak:[
+"Timbersaw",
+"Viper"
+]
+}
+},
+
+
+{
+name:"Wraith King",
+position:[1,3],
+image:"skeleton_king",
+counters:{
+strong:[
+"Phantom Assassin",
+"Anti-Mage",
+"Juggernaut"
+],
+weak:[
+"Ancient Apparition",
+"Viper",
+"Necrophos"
+]
+}
+},
+
+
+{
+name:"Phantom Lancer",
+position:[1],
+image:"phantom_lancer",
+counters:{
+strong:[
+"Sniper",
+"Medusa"
+],
+weak:[
+"Axe",
+"Earthshaker",
+"Legion Commander"
+]
+}
+},
+
+
+{
+name:"Naga Siren",
+position:[1],
+image:"naga_siren",
+counters:{
+strong:[
+"Spectre"
+],
+weak:[
+"Axe",
+"Ember Spirit",
+"Zeus"
+]
+}
+}
+
+];
