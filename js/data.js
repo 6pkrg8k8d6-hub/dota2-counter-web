0af23df505de2ad4a8dec63da2adb1ecb5b71266
6pkrg8k8d6-hub <6pkrg8k8d6@privaterelay.appleid.com>
2026-07-30 13:51:50 +1000

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const heroes = [
+
+{
+name:"Axe",
+counter:["Phantom Lancer","Naga Siren","Terrorblade"],
+countered:["Timbersaw","Necrophos"],
+role:"Offlane"
+},
+
+
+{
+name:"Doom",
+counter:["Storm Spirit","Puck","Morphling"],
+countered:["Viper","Silencer"],
+role:"Offlane"
+},
+
+
+{
+name:"Centaur",
+counter:["Anti Mage","Slark"],
+countered:["Ursa","Monkey King"],
+role:"Offlane"
+},
+
+
+{
+name:"Magnus",
+counter:["Troll","Sven"],
+countered:["Silencer"],
+role:"Offlane"
+}
+
+
+];
