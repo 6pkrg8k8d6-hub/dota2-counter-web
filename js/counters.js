2b41f0207abb051d2de8b0402d07d58702f5a736
6pkrg8k8d6-hub <6pkrg8k8d6@privaterelay.appleid.com>
2026-07-30 14:29:04 +1000

    Create counters.js

---
+++ js/counters.js
@@ -0,0 +1,144 @@
+const counters = {
+
+"Axe":{
+strong:[
+"Phantom Lancer",
+"Naga Siren",
+"Terrorblade",
+"Chaos Knight",
+"Meepo"
+],
+weak:[
+"Timbersaw",
+"Necrophos",
+"Viper",
+"Monkey King"
+]
+},
+
+
+"Doom":{
+strong:[
+"Storm Spirit",
+"Ember Spirit",
+"Puck",
+"Morphling",
+"Medusa"
+],
+weak:[
+"Silencer",
+"Viper",
+"Necrophos"
+]
+},
+
+
+"Centaur Warrunner":{
+strong:[
+"Anti-Mage",
+"Slark",
+"Phantom Assassin",
+"Ursa"
+],
+weak:[
+"Monkey King",
+"Viper",
+"Timbersaw"
+]
+},
+
+
+"Magnus":{
+strong:[
+"Sven",
+"Troll Warlord",
+"Phantom Assassin",
+"Juggernaut"
+],
+weak:[
+"Silencer",
+"Rubick",
+"Viper"
+]
+},
+
+
+"Tiny":{
+strong:[
+"Sniper",
+"Drow Ranger",
+"Crystal Maiden"
+],
+weak:[
+"Timbersaw",
+"Viper"
+]
+},
+
+
+"Wraith King":{
+strong:[
+"Phantom Assassin",
+"Anti-Mage",
+"Juggernaut"
+],
+weak:[
+"Ancient Apparition",
+"Viper"
+]
+},
+
+
+"Earthshaker":{
+strong:[
+"Phantom Lancer",
+"Naga Siren",
+"Meepo"
+],
+weak:[
+"Silencer",
+"Rubick"
+]
+},
+
+
+"Enigma":{
+strong:[
+"Phantom Lancer",
+"Chaos Knight",
+"Broodmother"
+],
+weak:[
+"Rubick",
+"Silencer",
+"Doom"
+]
+},
+
+
+"Zeus":{
+strong:[
+"Riki",
+"Broodmother",
+"Phantom Lancer"
+],
+weak:[
+"Storm Spirit",
+"Anti-Mage"
+]
+},
+
+
+"Phoenix":{
+strong:[
+"Phantom Lancer",
+"Chaos Knight",
+"Meepo"
+],
+weak:[
+"Silencer",
+"Ancient Apparition"
+]
+}
+
+};
